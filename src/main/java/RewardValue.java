c40064097aa653b9587783018401488ca7b5211a
prathamesh dhone <prathameshdone1413@gmail.com>
2024-02-10 01:44:52 +0530

    added overloaded constructor and updated conversion logic

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,10 +1,20 @@
 public class RewardValue {
-    final private double cashValue;
+     private double cashValue;
+     private int milesValue;
    public RewardValue(double cashValue){
+
        this.cashValue=cashValue;
+       this.milesValue=(int)Math.round((cashValue/0.0035d));
+
+
    }
+    public RewardValue(int milesValue){
+        this.milesValue=milesValue;
+        this.cashValue=milesValue*0.0035d;
+        this.cashValue=Math.round(this.cashValue*100)/100.0d;
+    }
    public double getMilesValue(){
-       return this.cashValue*0.0035d;
+       return this.milesValue;
    }
     public double getCashValue(){
         return this.cashValue;
